efdc75a770b3b7f46eaa786d5ab3b87c9fddca17
Ekophe <genz.millenials@gmail.com>
2023-06-05 12:57:29 +0700

    first commint

---
+++ app/layout.jsx
@@ -0,0 +1,44 @@
+// css
+import Nav from '@/components/Nav';
+import '../styles/globals.css';
+
+import {Bangers, Quicksand, Roboto_Condensed} from 'next/font/google';
+import CartMobileIcon from '@/components/CartMobileIcon';
+import CartProvider from '@/context/CartContext';
+import CartMobile from '@/components/CartMobile';
+import CartDesktop from '@/components/CartDesktop';
+import Footer from '@/components/Footer';
+
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  variable: '--font-quicksand',
+});
+
+const bangers = Bangers({
+  subsets: ['latin'],
+  variable: '--font-bangers',
+  weight: ['400']
+});
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ['latin'],
+  variable: '--font-robotoCondensed',
+  weight: ['300', '400', '700']
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <CartProvider>
+      <html lang='en'>
+        <body className={`${quicksand.variable} ${bangers.variable} ${robotoCondensed.variable} font-quicksand `}>
+          <Nav />
+          <CartMobileIcon />
+          <CartMobile />        
+          {children}
+          <CartDesktop />
+          <Footer />
+        </body>
+      </html>
+    </CartProvider>    
+  );
+}
---
+++ components/Pizza.jsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Modal from "react-modal";
+
+import PizzaDetails from "./PizzaDetails";
+
+import { IoCloseOutline } from "react-icons/io5";
+
+//bind modal to body
+Modal.setAppElement("body");
+
+//modal styles
+const modalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+};
+
+const Pizza = ({ pizza }) => {
+  // modal state
+  const [modal, setModal] = useState(false);
+  //open modal
+  const openModal = () => {
+    setModal(true);
+  };
+  //close modal
+  const closeModal = () => {
+    setModal(false);
+  };
+  return (
+    <div className="group py-2 px-4 xl:py-4 xl:px-2 rounded-xl">
+      <Image
+        className="lg:group-hover:translate-y-3 transition-all duration-300 mb-8 cursor-pointer"
+        width={270}
+        height={270}
+        src={pizza.image}
+        alt=""
+        priority={1}
+        onClick={openModal}
+      />
+
+      {/* title */}
+      <div onClick={openModal}>
+        <div className="text-xl font-bold mb-3 capitalize cursor-pointer">
+          {pizza.name}
+        </div>
+      </div>
+
+      {/* description */}
+      <div className="text-sm font-medium min-h-[60px] mb-6">
+        {pizza.description}
+      </div>
+
+      {/* prince & btn */}
+      <div className="mb-6 flex items-center justify-between">
+        {/* price -> hidden(sm) - visible (lg) */}
+        <div className="hidden lg:flex text-xl font-semibold">
+          starts at {pizza.priceSm}
+        </div>
+        {/* btn -> hidden(sm) - visible (lg) */}
+        <button
+          onClick={openModal}
+          className="hidden lg:flex gradient text-white rounded-lg btn-sm font-semibold text-sm"
+        >
+          Choose{" "}
+        </button>
+        {/* button -> visible (sm) - hidden (lg) */}
+        <button
+          onClick={openModal}
+          className="btn btn-sm gradient text-sm lg:hidden px-3"
+        >
+          starts at {pizza.priceSm}
+        </button>
+      </div>
+      {/* modal */}
+      {modal && (
+        <Modal
+          isOpen={modal}
+          style={modalStyles}
+          onRequestClose={closeModal}
+          contentLabel="Pizza Modal"
+          className="bg-white w-full h-full lg:max-w-[900px] lg:max-h-[600px] lg:rounded-[30px] lg:fixed lg:top-[50%] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%] outline-none"
+        >
+          {/* close modal icon */}
+          <div
+            onClick={closeModal}
+            className="absolute z-30 right-2 top-2 hover:scale-110 duration-200 cursor-pointer"
+          >
+            <IoCloseOutline className="text-4xl text-orange" />
+          </div>
+
+          {/* pizza details */}
+          <PizzaDetails pizza={pizza} modal={modal} setModal={setModal} />
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default Pizza;
